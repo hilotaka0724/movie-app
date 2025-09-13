1d6a4f51fbc8e76c2b28e5acac1b26338a04d796
hilotala0724 <hiiro000724@gmail.com>
2025-09-13 11:26:51 +0900

    環境構築

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  alert("Hello World");
+  return (
+    <>
+      <div>
+        <h1>Helllo World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
