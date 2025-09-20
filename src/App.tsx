e346201b614c1cdef7a036e78d259ded098497ba
hilotala0724 <hiiro000724@gmail.com>
2025-09-20 22:22:23 +0900

    映画APIからデータ取得ロジック実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,73 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const defaultMovieList = [
     {
       id: 1,
-      name: "君の名は",
+      original_title: "君の名は",
       image:
         "https://media.themoviedb.org/t/p/w300_and_h450_bestv2/yLglTwyFOUZt5fNKm0PWL1PK5gm.jpg",
-      overView:
+      overview:
         "1ヵ月後に1000年ぶりの彗星が訪れる日本。東京で暮らす平凡な男子高校生・瀧と、山深い村で都会の生活に憧れながら憂鬱な日々を送る女子高校生・三葉。つながりのない2人は、互いが入れ替わる不思議な夢を見る。",
     },
     {
       id: 2,
-      name: "ハウルの動く城",
+      original_title: "ハウルの動く城",
       image:
         "https://media.themoviedb.org/t/p/w300_and_h450_bestv2/v0K2e1t6ocUNnkZ9BeiFdcOT9LG.jpg",
     },
     {
       id: 3,
-      name: "もののけ姫",
+      original_title: "もののけ姫",
       image:
         "https://media.themoviedb.org/t/p/w300_and_h450_bestv2/mVdz3vlmioKWZaHTGfu99zIuayZ.jpg",
     },
     {
       id: 4,
-      name: "バック・トゥ・ザ・フューチャー",
+      original_title: "バック・トゥ・ザ・フューチャー",
       image:
         "https://media.themoviedb.org/t/p/w300_and_h450_bestv2/oHaxzQXWSvIsctZfAYSW0tn54gQ.jpg",
     },
   ];
 
+  const fechMovieList = async () => {
+    const url =
+      "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
+      },
+    };
+    const response = await fetch(url, options);
+    const data = await response.json();
+    console.log(data.results);
+    setMovieList(data.results);
+  };
+
+  useEffect(() => {
+    fechMovieList();
+  });
+
   const [keyword, setKeyword] = useState<string>("");
+  // const [movieList, setMovieList] = useState<object>([]);
+  const [movieList, setMovieList] = useState([defaultMovieList]);
   return (
     <>
       <div>{keyword}</div>
       <input type="text" onChange={(e) => setKeyword(e.target.value)} />
-      {defaultMovieList
-        .filter((movie) => movie.name.includes(keyword))
+      {movieList
+        .filter((movie) => movie.original_title.includes(keyword))
         .map((movie) => (
           <div key={movie.id}>
-            <h2>{movie.name}</h2>
-            <img src={movie.image} alt={movie.name} />
-            <p>{movie.overView}</p>
+            <h2>{movie.original_title}</h2>
+            <img
+              src={`http://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.original_title}
+            />
+            <p>{movie.overview}</p>
           </div>
         ))}
     </>
